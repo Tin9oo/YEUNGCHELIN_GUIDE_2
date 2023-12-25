08f20056879c107d526f89bcb5b0481f1eae26c6
amitheson <78153302+amitheson@users.noreply.github.com>
2023-12-25 22:17:01 +0900

    231225

--- yc_guide/src/components/Restaurant_detail.js
+++ yc_guide/src/components/Restaurant_detail.js
@@ -44,20 +44,23 @@ export default function RestaurantDetailPopup(props) {
   const [editButtonHit, setEditButtonHit] = useState(1);
 
   const [editingRestInfo, setEditingRestInfo] = useState({
-    ID: restInfo.idrestaurant,
-    name: restInfo.name,
-    category1: restInfo.category1,
-    category2: restInfo.category2,
-    telnum: restInfo.telnum,
-    coarse_location: restInfo.coarse_location,
-    real_location: restInfo.real_location,
-    operation_hour: restInfo.operation_hour,
-    breakingtime: restInfo.breakingtime,
-    update_date: restInfo.update_date,
+    // ID: restInfo.idrestaurant,
+    // name: restInfo.name,
+    // category1: restInfo.category1,
+    // category2: restInfo.category2,
+    // telnum: restInfo.telnum,
+    // coarse_location: restInfo.coarse_location,
+    // real_location: restInfo.real_location,
+    // operation_hour: restInfo.operation_hour,
+    // breakingtime: restInfo.breakingtime,
+    // update_date: restInfo.update_date,
   });
-  const [editable, setEditable] = useState(false); //수정 가능 여부
 
-  const [saveButtonHit, setSaveButtonHit] =useState(1);
+  const [editable, setEditable] = useState(false); //수정 가능 여부
+  const [saveButtonHit, setSaveButtonHit] = useState(1);
+
+  const [editingMenu, setEditingMenu] = useState([]);
+  const [editableMenu, setEditableMenu] = useState(false);
 
   useEffect(() => {
     RestInfo();
@@ -80,10 +83,23 @@ export default function RestaurantDetailPopup(props) {
       .then((response) => response.json())
       .then((data) => {
         setMenuInfo(data);
-        console.log("Menu Info:", data);
+        setEditingMenu(data); // 가져온 데이터로 editingMenu 초기화
       })
-      .catch((error) => console.log("Error fetching data: ", error));
+      .catch((error) => console.log("데이터를 불러오는 중 에러 발생: ", error));
+
+    console.log("MenuInfo setMenuInfo", editingMenu);
+    console.log("length", editingMenu.length)
   };
+
+  // const MenuInfo = () => {
+  //   fetch(menuUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setMenuInfo(data);
+  //       console.log("Menu Info:", data);
+  //     })
+  //     .catch((error) => console.log("Error fetching data: ", error));
+  // };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -135,7 +151,7 @@ export default function RestaurantDetailPopup(props) {
 
   const saveButton = () => {
     updateRestaurantInfo();
-    setSaveButtonHit(saveButtonHit+1);
+    setSaveButtonHit(saveButtonHit + 1);
     // console.log("saveButtonHit", saveButtonHit);
     setEditable(false);
   };
@@ -143,7 +159,7 @@ export default function RestaurantDetailPopup(props) {
   useEffect(() => {
     const now = new Date();
     const formattedDate = now.toISOString().split('T')[0];
-    console.log("시간",now);
+    console.log("시간", now);
     console.log("Restname when save Button hitted", Restname);
     setEditingRestInfo((prevEditingRestInfo) => ({
       ...prevEditingRestInfo,
@@ -174,6 +190,20 @@ export default function RestaurantDetailPopup(props) {
         console.log("Data updated successfully:", data);
       })
       .catch((error) => console.error("Error updating data:", error));
+  };
+
+
+  const handleLikesCount = () => {
+    //LIKE 버튼 눌렀을 때
+  };
+
+  const handleMenuChange = (index, field, value) => {
+    //setEditingMenu
+    setEditableMenu ((prevEditingMenu) => {
+      const updatedMenu = [...prevEditingMenu];
+      updatedMenu[index][field] = value;
+      return updatedMenu;
+    });
   };
 
   if (editable) {
@@ -223,7 +253,7 @@ export default function RestaurantDetailPopup(props) {
             label="category1"
             //value={restInfo[0].category1}
             value={Restcategory1}
-            onChange={(e) => setCategory1(e.target.value )}
+            onChange={(e) => setCategory1(e.target.value)}
             variant="standard"
           />
           <TextField
@@ -250,7 +280,7 @@ export default function RestaurantDetailPopup(props) {
             label="coarse_location"
             //value={restInfo[0].coarse_location}
             value={RestcoarseLocation}
-            onChange={(e) =>setCoarseLocation(e.target.value )}
+            onChange={(e) => setCoarseLocation(e.target.value)}
             variant="standard"
           />
           <TextField
@@ -259,7 +289,7 @@ export default function RestaurantDetailPopup(props) {
             label="real_location"
             //value={restInfo[0].real_location}
             value={RestrealLocation}
-            onChange={(e) =>setRealLocation(e.target.value )}
+            onChange={(e) => setRealLocation(e.target.value)}
             variant="standard"
           />
           <TextField
@@ -268,7 +298,7 @@ export default function RestaurantDetailPopup(props) {
             label="operation_hour"
             //value={restInfo[0].operation_hour}
             value={RestoperationHour}
-            onChange={(e) =>setOperationHour(e.target.value )}
+            onChange={(e) => setOperationHour(e.target.value)}
             variant="standard"
           />
           <TextField
@@ -277,7 +307,7 @@ export default function RestaurantDetailPopup(props) {
             label="breaking_time"
             //value={restInfo[0].breakingtime}
             value={RestbreakingTime}
-            onChange={(e) =>setBreakingTime(e.target.value )}
+            onChange={(e) => setBreakingTime(e.target.value)}
             variant="standard"
           />
 
@@ -286,28 +316,111 @@ export default function RestaurantDetailPopup(props) {
             <ListItem>
               <ListItemText primary="메뉴" />
             </ListItem>
-            {menuInfo &&
-              menuInfo.map((menu, index) => (
+            {editingMenu &&
+              editingMenu.map((menu, index) => (
                 <ListItem key={index}>
                   <>
                     <TextField
                       required
-                      id="standard-required"
+                      id={`menu-name-${index}`}
                       label="메뉴명"
-                      defaultValue={menu.name}
+                      value={menu.name}
+                      onChange={(e) => handleMenuChange(index, 'name', e.target.value)}
+                      variant="standard"
+                    />
+                    <TextField
+                      required
+                      id={`menu-price-${index}`}
+                      label="가격"
+                      value={menu.price}
+                      onChange={(e) => handleMenuChange(index, 'price', e.target.value)}
                       variant="standard"
                     />
                     <TextField
                       required
                       id="standard-required"
-                      label="가격"
-                      defaultValue={menu.price}
+                      label="LIKES"
+                      value={menu.likes}
+                      onChange={(e) => handleMenuChange(index, 'likes',e.target.value)}
+                      //defaultValue={menu.price}
                       variant="standard"
                     />
                   </>
                 </ListItem>
               ))}
           </List>
+
+          {/* <List>
+            <Divider/>
+            <ListItem>
+              <ListItemText primary="메뉴" />
+            </ListItem>
+
+            {editingMenu && editingMenu.map((menu, index) => (
+              <ListItem key={index}>
+                <>
+                  <TextField
+                    required
+                    id="standard-required"
+                    label="메뉴명"
+                    //defaultValue={menu.name}
+                    value={menu.name}
+                    onChange={(e) => setBreakingTime(e.target.value)}
+                    variant="standard"
+                  />
+                  <TextField
+                    required
+                    id="standard-required"
+                    label="가격"
+                    value={menu.price}
+                    onChange={(e) => setBreakingTime(e.target.value)}
+                    //defaultValue={menu.price}
+                    variant="standard"
+                  />
+                  <TextField
+                    required
+                    id="standard-required"
+                    label="LIKES"
+                    value={menu.likes}
+                    onChange={(e) => setBreakingTime(e.target.value)}
+                    //defaultValue={menu.price}
+                    variant="standard"
+                  />
+                </>
+              </ListItem>
+            ))}
+          </List> */}
+
+          {/* <List>
+            <Divider />
+            <ListItem>
+              <ListItemText primary="메뉴" />
+            </ListItem>
+            {menuInfo && menuInfo.map((menu, index) => (
+                <ListItem key={index}>
+                  <>
+                    <TextField
+                      required
+                      id="standard-required"
+                      label="메뉴명"
+                      //defaultValue={menu.name}
+                      value={menu.name}
+                      onChange={(e) =>setBreakingTime(e.target.value)}
+                      variant="standard"
+                    />
+                    <TextField
+                      required
+                      id="standard-required"
+                      label="가격"
+                      value={menu.price}
+                      onChange={(e) =>setBreakingTime(e.target.value)}
+                      //defaultValue={menu.price}
+                      variant="standard"
+                    />
+                  </>
+                </ListItem>
+              ))}
+          </List> */}
         </Dialog>
       </React.Fragment>
     );
@@ -373,6 +486,8 @@ export default function RestaurantDetailPopup(props) {
                   <>
                     <ListItemText primary={menu.name} />
                     <ListItemText primary={menu.price} />
+                    <ListItemText primary={menu.likes} />
+                    <Button autoFocus color="inherit" onClick={handleLikesCount}>I LIKE IT</Button>
                   </>
                 </ListItem>
               ))}
